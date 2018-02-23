e6e7512e32033d7aed72806e217201c164d473de
kchen36 <kchen36@stuy.edu>
2018-02-23 13:25:44 -0500

    done

--- mongo.py
+++ mongo.py
@@ -7,5 +7,18 @@ client = MongoClient("homer.stuy.edu")
 db = client['pokedex']
 collection = db.pokemon
 for i in pokedex:
-    collection.insert_one(pokedex)
+    collection.insert_many(pokedex["pokemon"])
 
+def find(pokemon):
+    return collection.find({"name" : pokemon})
+
+def findid(num):
+    return collection.find({"id" : num})
+
+def findevo(name):
+    return collection.find({"next_evolutionname" : name})
+def p(x):
+    for i in x:
+	print i
+
+p(find("Charmander"))
