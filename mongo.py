502c77dbfdcf266b0a3c9245656f02afd8e2d625
Kerry Chen <kchen36@stuy.edu>
2018-02-26 14:06:38 -0500

    working on it

--- mongo.py
+++ mongo.py
@@ -1,9 +1,19 @@
 from pymongo import MongoClient
 import json
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/search')
+def search():
+    return render_template("base.html")
+@app.route('/pokemon')
+def pokemon():
+    return render_template("base.html"
 file = open("pokedex.json", 'r')
 pokedex = json.load(file)
 client = MongoClient("homer.stuy.edu")
 
+db = client['pokedex']
+db.dropDatabase()
 db = client['pokedex']
 collection = db.pokemon
 for i in pokedex:
@@ -16,9 +26,15 @@ def findid(num):
     return collection.find({"id" : num})
 
 def findevo(name):
-    return collection.find({"next_evolutionname" : name})
+    return collection.find({"next_evolution.name" : name})
+
+def findtype(name):
+    return collection.find({"type" : name})
+
 def p(x):
     for i in x:
 	print i
-
-p(find("Charmander"))
+    
+if __name__ == "__main__":
+	app.debug = True
+    app.run()
